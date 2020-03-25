9d40cf3eb4a00b44edf8c47f3d47abaccc9c056f
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-03-25 16:53:26 +0900

    Update Object_IOStream_miniProject_Accessable.java

--- IOStream_miniProject_01_extended/Object_IOStream_miniProject_Accessable.java
+++ IOStream_miniProject_01_extended/Object_IOStream_miniProject_Accessable.java
@@ -1,5 +1,3 @@
-package 입출력연습;
-
 public interface Object_IOStream_miniProject_Accessable {
 	void saveSchool(School school, String fileName)throws Exception;
 	void restoreSchool(School school, String fileName)throws Exception;
